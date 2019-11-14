e50bd91748f1d269ca013c7a2c368920890e8daf
Patrick Mahon <p.mahon0628@gmail.com>
2019-11-13 21:47:03 -0500

    add

--- beach/src/App.js
+++ beach/src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/home'
+import Rooms from './pages/rooms'
+import SingleRoom from './pages/singleRoom'
+import Error from './pages/Error'
+
+import {Route, Switch} from 'react-router-dom'
+
+import Navbar from "./components/Navbar";
 
 
+function App(){
+    return (
+        <>
+        <Navbar />
+        <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+        </Switch>
+      </>
+    );
+}
 export default App;
