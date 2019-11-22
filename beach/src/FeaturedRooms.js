dc917aa691ad34dde8ef56886f8201e3a37b41a0
Patrick Mahon <p.mahon0628@gmail.com>
2019-11-21 21:37:15 -0500

    rooms page

--- beach/src/FeaturedRooms.js
+++ beach/src/FeaturedRooms.js
@@ -1,16 +1,25 @@
 import React, { Component} from 'react';
 import { RoomContext } from "./context";
+// import Loading from "./Loading";
+// import Room from './Room';
+import Title from '.';
  
-export default class FeaturedRooms extends
-Component{
+export default class FeaturedRooms extends Component{
     static contextType = RoomContext
     render(){
-        const{name, greeting} = this.context;
-
-        return(
-            <div>
-                featured  rooms{value}
+        const{loading, FeaturedRooms : rooms} = 
+        this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room}
+     })
+           
+         return(
+            <section className="featured-rooms">
+                <Title title="featured-rooms"/>
+                <div  className="featured-rooms-center">
+                {}
+                </section>
             </div>
-        )
+        );
     }
 }
--- beach/src/context.js
+++ beach/src/context.js
@@ -4,9 +4,38 @@ const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
 state={
-    rooms: []
+    rooms: [],
+    sortedRooms:[],
+    featuredRooms:[],
+    loading: true
 };
-    render(){
+// getData
+
+componentDidMount(){
+    let rooms = this.formatData(items)
+    let featuredRooms = rooms.filter(room = room.featured
+        === true);
+    this.setState({
+        rooms, 
+        featuredRooms,  
+        sortedRooms:rooms,
+        loading: false
+    });
+   }
+
+formatData(items){
+    // this.getData
+ let tempItems  = items.map(item =>{
+    let id  = item.sys.id
+    let images = item.fields.images.map(image => 
+            image.fields.files.url);
+
+    let rooms = {...item.fields,images,id}
+    return rooms; 
+    });
+}
+
+render(){
         return <RoomContext.Provider value={{ ...this.state }}>
         {this.props.children}
         </RoomContext.Provider>
