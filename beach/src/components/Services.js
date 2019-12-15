b6d2b19eea5dd07009d5052f4efdd03852e3c5ad
Patrick Mahon <p.mahon0628@gmail.com>
2019-12-14 23:28:40 -0500

    removing some ipsum

--- beach/src/components/Services.js
+++ beach/src/components/Services.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
-import {FaCocktail, FaHiking, FaShuttleVan, FaBeer}
-from "react-icons/fa";
-import Title from '../components/Title'
-
+import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import Title from "./Title";
 export default class Services extends Component {
-    state={
-        servics: [
-       {
-        icon:<FaCocktail/>,
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
         title: "Free Cocktails",
-        info:"Every day from 4-5 come enjoy some delicious cocktails at our happy hour"
-        },
-        {
-         icon:<FaHiking/>,
-         title:"The Best Hiking around!",
-         info: "Come expolore some of the fantastic hikes and trails around!"
-        },
-        {
-         icon:<FaShuttleVan/>,
-         title:"Complimentary Shuttle!",
-         info:"Free Shuttle, ask the front desk for more details!"
-        },
-        {
-         icon:<FaBeer/>,
-         title:"Great local Beer",
-         info:"We make our own beers on property, come check out the variety!"
-        }
-     ]
-    }
-    render() {
-        return (
-          <section className="services">
-            <Title title="services" />
-            <div className="services-center">
-              {this.state.services.map(item => {
-                return (
-                  <article key={`item-${item.title}`} className="service">
-                    <span>{item.icon}</span>
-                    <h6>{item.title}</h6>
-                    <p>{item.info}</p>
-                  </article>
-                );
-              })}
-            </div>
-          </section>
-        );
+        info:
+          "Come join us for happy hour craft cocktails in the lobby from 5 - 7 PM everyday!"
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "We are surrounded by over twenty five miles of amazing trails and hikes!"
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free Shuttle",
+        info:
+          "Need a lift into town, the airport?  Call the front desk to arrange shuttle transport"
+      },
+      {
+        icon: <FaBeer />,
+        title: "Homeade Beer",
+        info:
+          "We have a brewery on site that makes five different kinds of beer.  Come and enjoy."
       }
-    } 
-
+    ]
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map(item => {
+            return (
+              <article key={`item-${item.title}`} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
 
 // component that has various services the property offers. set the state with the 
 // different items and then pass them using the span below.
