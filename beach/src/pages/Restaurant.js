4cb3c2b90afa608caf5767e73aad36fce1294979
Patrick Mahon <p.mahon0628@gmail.com>
2019-12-14 23:33:29 -0500

    some scaffolding

---
+++ beach/src/pages/Restaurant.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+import Button from '../components/StyledHero';
