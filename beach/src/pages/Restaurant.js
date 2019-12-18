1a64d93bfc76ee8c9b2dbcf97c336b7422298d23
Patrick Mahon <p.mahon0628@gmail.com>
2019-12-17 21:30:28 -0500

    scaffolding new page

--- beach/src/pages/Restaurant.js
+++ beach/src/pages/Restaurant.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import Services from "../components/Services";
-import FeaturedRooms from "../components/FeaturedRooms";
-import Button from '../components/StyledHero';
+
+// import Hero from "../components/Hero";
+// import Banner from "../components/Banner";
+// import Services from "../components/Services";
+// import FeaturedRooms from "../components/FeaturedRooms";
+// import Button from '../components/StyledHero';
