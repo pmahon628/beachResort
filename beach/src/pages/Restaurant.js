1fc2a53f60778531128ab32027395d232cfee76e
Patrick Mahon <p.mahon0628@gmail.com>
2019-12-17 22:18:53 -0500

    more work

--- beach/src/pages/Restaurant.js
+++ beach/src/pages/Restaurant.js
@@ -3,8 +3,21 @@ import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 
-// import Hero from "../components/Hero";
-// import Banner from "../components/Banner";
-// import Services from "../components/Services";
-// import FeaturedRooms from "../components/FeaturedRooms";
-// import Button from '../components/StyledHero';
+const Restaurant = () => {
+    return (
+      <>
+        <Hero>
+          <Banner
+            title="Voted Top Burgers"
+            subtitle="Come check out the best food on the mountain"
+          >
+              <Link to="/" className="btn-primary">
+            return home
+          </Link>
+             </Banner>
+        </Hero>
+        </>
+    );
+  };
+  
+  export default Restaurant;
