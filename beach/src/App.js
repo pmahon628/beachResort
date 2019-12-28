80e0bffe04ed01538a4e1c30a4ba2808effb7074
Patrick Mahon <p.mahon0628@gmail.com>
2019-12-28 11:15:06 -0500

    routing works

--- beach/src/App.js
+++ beach/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import './App.css';
-
-import Home from './pages/home'
-import Rooms from './pages/rooms'
-import SingleRoom from './pages/singleRoom'
-import Error from './pages/Error'
-
-import {Route, Switch} from 'react-router-dom'
-
+import React from "react";
+import "./App.css";
+import Home from "./pages/home";
+import Rooms from "./pages/rooms";
+import SingleRoom from "./pages/singleRoom";
+import Restaurant from "./pages/Restaurant";
+import Error from "./pages/Error";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-
-function App(){
-    return (
-        <>
-        <Navbar />
-        <Switch>
-        <Route exact path="/" component={Home}/>
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
         <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route exact path="/Restaurant" component={Restaurant} />
         <Route component={Error} />
-        </Switch>
-      </>
-    );
+      </Switch>
+    </>
+  );
 }
 export default App;
